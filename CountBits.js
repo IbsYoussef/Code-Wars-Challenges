5ae00db4a12d8fed5dac4aeb9924082c7ae76fd0
IbsYoussef <124460805+IbsYoussef@users.noreply.github.com>
2023-02-04 23:06:27 +0000

    Create CountBits.js

---
+++ CountBits.js
@@ -0,0 +1,33 @@
+// Teamates Solution
+var countBits = function(n) {
+    let count = 0
+    let binary = (n >>> 0).toString(2)
+    for (i = 0; i < n.toString().length; i++) {
+        if (n[i] = "1") {
+            count ++
+        }
+    }
+    console.log(count)
+    return count
+  
+};
+
+function decimalToBinary(decimal) {
+  return (decimal >>> 0).toString(2)
+}
+
+console.log(decimalToBinary(14))
+
+// My solution
+var countBits = function(n) {
+    let x = (n).toString(2)
+    let nw = x.split('')
+    let final = 0
+
+    for (let i = 0; i < nw.length; i++){
+        final += Number(nw[i])
+    }
+    return final
+  };
+
+  console.log(countBits(1234))
