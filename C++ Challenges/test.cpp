5109d4a7334461a9cab68300efb12d80ef69459b
IbsYoussef <ibraheemyoussefiy@gmail.com>
2023-03-18 23:41:07 +0000

    Removed Functions

--- C++ Challenges/test.cpp
+++ C++ Challenges/test.cpp
@@ -1,22 +1,6 @@
 #include <iostream>
 using namespace std;
 
-int Recursion(int n){
-    if (n == 1){
-        return 1;
-    } else {
-        return n * Recursion(n-1);
-    }
-}
-
-int Recursion2(int n){
-      if (n == 1){
-        return 1;
-    } else {
-        return n + Recursion(n-1);
-    }
-}
-
 int main()
 {
 
