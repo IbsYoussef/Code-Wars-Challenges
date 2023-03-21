4503631db632a686138e4b0d72b134a8d906b84c
IbsYoussef <ibraheemyoussefiy@gmail.com>
2023-03-21 15:54:53 +0000

    Sum mixed array . go

---
+++ Go-Kata/summixedarray.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"strconv"
+)
+
+func SumMix(arr []any) int {
+	sum := 0
+
+	for _, v := range arr {
+		switch v.(type) {
+		case int:
+			sum += v.(int)
+		case string:
+			s, _ := strconv.Atoi(v.(string))
+			sum += s
+		}
+	}
+	return sum
+
+}
