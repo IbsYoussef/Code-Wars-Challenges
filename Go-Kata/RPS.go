fa74bd784091e0e7646ad065f02311f6cf322d02
IbsYoussef <ibraheemyoussefiy@gmail.com>
2023-03-21 15:12:56 +0000

    RPS.go re-edit

--- Go-Kata/RPS.go
+++ Go-Kata/RPS.go
@@ -5,16 +5,23 @@ import (
 )
 
 func main() {
-
+	// RPS 1 function
 	fmt.Println(Rps("rock", "scissors"))     // P1 WINS
 	fmt.Println(Rps("rock", "paper"))        // P1 WINS
 	fmt.Println(Rps("paper", "scissors"))    // P2 WINS
 	fmt.Println(Rps("rock", "paper"))        // P2 WINS
 	fmt.Println(Rps("scissors", "scissors")) // DRAW
 
+	// RPS 2 function
+	fmt.Println(Rps2("rock", "scissors"))     // P1 WINS
+	fmt.Println(Rps2("scissors", "paper"))    // P1 WINS
+	fmt.Println(Rps2("paper", "paper"))       // DRAW
+	fmt.Println(Rps2("scissors", "scissors")) // DRAW
+	fmt.Println(Rps2("paper", "rock"))        // P1 WINS
+
 }
 
-func Rps(p1, p2 string) string {
+func Rps(p1, p2 string) string { // My Solution
 
 	if p1 == "rock" && p2 == "scissors" {
 		return "Player 1 won!"
@@ -43,4 +50,17 @@ func Rps(p1, p2 string) string {
 	} else {
 		return "Draw!"
 	}
+}
+
+func Rps2(p1, p2 string) string { // Solution I found
+
+	var m = map[string]string{"rock": "paper", "paper": "scissors", "scissors": "rock"}
+
+	if p1 == p2 {
+		return "Draw!"
+	}
+	if m[p1] == p2 {
+		return "Player 2 won!"
+	}
+	return "Player 1 won!"
 }
